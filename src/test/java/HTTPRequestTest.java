3f346376ff52c2cc31762b21a54470ff7c07b476
TravisRidge <Travis.Ridge96@gmail.com>
2019-02-06 10:13:25 -0500

    Added web tests

---
+++ src/test/java/HTTPRequestTest.java
@@ -0,0 +1,28 @@
+import App.Greeting;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HTTPRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assert(this.restTemplate.getForObject("http://localhost:" + port + "/greeting",
+                Greeting.class).getContent().contains("Hello, World!"));
+    }
+
+}
